18d31d7db5c58b17e53b15213f5af651361d023e
HadayaRahman-SF4673 <hadayarahman.m@syncfusion.com>
2025-03-04 17:52:47 +0530

    934406-Added-Correction

--- CarDashBoard/MauiProgram.cs
+++ CarDashBoard/MauiProgram.cs
@@ -14,6 +14,7 @@ namespace CarDashBoard
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("CarDashBoard.ttf", "CarDashboard");
                 });
 
 #if DEBUG
