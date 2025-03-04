b3f99716ebd5ae77c4d31025336bcf6cc5166d9e
HadayaRahman-SF4673 <hadayarahman.m@syncfusion.com>
2025-03-04 11:45:38 +0530

    934406-Added-Initial-Project

---
+++ CarDashBoard/Behavior/MainPageBehavior.cs
@@ -0,0 +1,40 @@
+﻿using Syncfusion.Maui.Gauges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDashBoard
+{
+    public class MainPageBehavior : Behavior<ContentPage>
+    {
+        private RadialAxis? radialAxis;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            radialAxis = bindable.FindByName<RadialAxis>("temperatureAxis");
+
+            radialAxis.LabelCreated += RadialAxis_LabelCreated;
+        }
+
+        private void RadialAxis_LabelCreated(object? sender, LabelCreatedEventArgs e)
+        {
+            if (int.Parse(e.Text) == 75 || int.Parse(e.Text) == 125 || int.Parse(e.Text) == 175)
+            {
+                e.Text = string.Empty;
+            }
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            if (radialAxis != null)
+            {
+                radialAxis.LabelCreated -= RadialAxis_LabelCreated;
+                radialAxis = null;
+            }
+        }
+    }
+}
---
+++ CarDashBoard/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CarDashBoard
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
